fe9d80abf8eb7a18740da3f626ac7409838f6ed4
MAHESH KUMAR.D <maheshkumar.d@Mahesh-Kumar-D-Macbook-Air-15.local>
2025-02-15 17:21:31 +0530

    qwerty

--- nexus/code/vector_store.py
+++ nexus/code/vector_store.py
@@ -3,7 +3,7 @@ from time import sleep
 from xml.etree.ElementTree import PI
 from dotenv import load_dotenv
 import pinecone
-from langchain.embeddings import OpenAiEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorspace import Pinecone
 import numpy as np
 
@@ -16,15 +16,15 @@ pinecone.init(
 class VectoStore:
     def __init__(self):
         self.index_name = "nex-dosuments"
-        self.embeddings = OpenAiEmbeddings(openai_api_key=os.getenv("OPEN_API_KEY"))
+        self.embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPEN_API_KEY"))
         if (self.index_name) not in pinecone.list_collections():
             pinecone.create_index(
                 name=self.index_name,
-                dimensios=1536,
+                dimensions=1536,
                 metric="cosine",
             )
         self.index = pinecone.Index(self.index_name)
-        self.vectorstore = Pinecone(self.index, self.embeddings.embedded_query, "text")
+        self.vectorstore = Pinecone(self.index, self.embeddings.embed_query, "text")
 
     def store_document(self, file_data, metadata):
         text_content = self._convert_to_text(file_data)
