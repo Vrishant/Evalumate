f942292b4af5ae57df17d604dba90accbe2ef3d6
MrAstatine <akshatkrish2005@gmail.com>
2025-02-15 14:29:20 +0530

    start reforms

--- nexus/code/main.py
+++ nexus/code/main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+"""
+@app.get("/doc")
+def newOne():
+    return {"message": "Hello, World!"}
+"""
+
+
+@app.post("/document/upload")
+def upload_file():
+    pass
---
+++ nexus/code/models.py
@@ -0,0 +1,14 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, LargeBinary, DateTime
+from datetime import datetime
+
+
+class Nex_doc(Base):
+    __tablename__ = "nex_doc"
+    id = Column(Integer, primary_key=True, index=True)
+    chatroom_No = Column(Integer)
+    file_name = Column(String, nullable=False)
+    file_data = Column(LargeBinary, nullable=False)
+    file_type = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
